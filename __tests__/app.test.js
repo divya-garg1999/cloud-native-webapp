124b745fc1d5c82435970785fe9caeda23eaa2a4
divyagarg <divya.garg6543@gmail.com>
2026-02-23 11:14:15 +0530

    Day 4: Add Jest test stage

---
+++ __tests__/app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../server');
+
+describe('GET /', () => {
+  it('should return 200 OK and Hello World', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('Hello World!');
+  });
+});
