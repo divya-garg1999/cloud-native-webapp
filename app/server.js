e288f56819db3c3fe1b58c17d5598a8c348e2ce1
divyagarg <divya.garg6543@gmail.com>
2026-03-06 13:29:37 +0530

    Day 9: updated API URL to myapp-service ELB

--- app/server.js
+++ app/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 
+// Root route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// API route
+app.get('/api', (req, res) => {
+  res.json({ message: "Hello World! Cloud Native Resume Project" });
+});
+
+// Start server
 if (require.main === module) {
-  app.listen(3000, () => console.log('App running on port 3000'));
+  app.listen(3000, '0.0.0.0', () => {
+    console.log('App running on port 3000');
+  });
 }
 
 module.exports = app;
