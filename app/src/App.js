e77606a8e9a9fddd18482eca97e7e47a96da6982
divyagarg <divya.garg6543@gmail.com>
2026-03-06 14:02:58 +0530

    Day 10: removed API response section from React frontend

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1>Cloud Native WebApp</h1>
+      <p>Hello World from React!</p>
+      <p>This frontend is hosted on Netlify with HTTPS.</p>
+      <p>The backend API is running on AWS ELB.</p>
+    </div>
+  );
+}
+
+export default App;
+
+
